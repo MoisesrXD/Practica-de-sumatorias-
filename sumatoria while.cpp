2f45628ecd9ac7cf0353fa692123052d2df4cf47
Moises Roman <64390156+MoisesrXD@users.noreply.github.com>
2021-07-05 09:10:32 -0500

    Create sumatoria while.cpp

---
+++ sumatoria while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int k= 1,n, r,total;
+		cin >> n;
+		while (k <= n)
+	{
+		r= r +( 2 * k) ;     //edite la sumatoria
+				cout << r << endl;
+			k++;
+    
+    return 0;
+    }
